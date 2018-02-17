8a963d73a82edeef7c2c0e784e8285cef76305f4
kimshuye <suntisuk.go@gmail.com>
2018-02-17 23:19:04 +0700

    Set All Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,32 @@ import { HomeComponent } from './components/home/home.component';
 import { FormsModule } from "@angular/forms";
 
 
+import { ProductsComponent } from './components/products/products.component';
+import { ProductComponent } from './components/product/product.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { EditProductComponent } from './components/edit-product/edit-product.component';
+import { DeleteProductComponent } from './components/delete-product/delete-product.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProductsComponent,
+    ProductComponent,
+    AddProductComponent,
+    EditProductComponent,
+    DeleteProductComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
   providers: [],
-  bootstrap: [HomeComponent]
-  //bootstrap: [AppComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
